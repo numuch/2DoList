5f7d3785cf70e5058ef4545c9e9eb461b8b8804b
numuch <tbnoon@gmail.com>
2017-07-23 20:44:30 +0900

    ステータス表示

--- main/java/com/tb_system/todolist/StatusDialog.java
+++ main/java/com/tb_system/todolist/StatusDialog.java
@@ -12,7 +12,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
-
 import static com.tb_system.todolist.Cont.DateFormat;
 import static com.tb_system.todolist.Cont.*;
 import static com.tb_system.todolist.Cont.makeSentence;
