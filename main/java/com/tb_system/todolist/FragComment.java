57237f345cd671de64a2a65f38783ccfaf0dc0a1
numuch <tbnoon@gmail.com>
2017-07-23 20:42:17 +0900

    キャラがコメントを表示する

--- main/java/com/tb_system/todolist/FragComment.java
+++ main/java/com/tb_system/todolist/FragComment.java
@@ -15,7 +15,6 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
