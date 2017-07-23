c7069b931f9686aa6d395f45eb991e5644aa8ed6
numuch <tbnoon@gmail.com>
2017-07-23 20:44:18 +0900

    ナンバーピッカー

--- main/java/com/tb_system/todolist/NumberPickerDialog.java
+++ main/java/com/tb_system/todolist/NumberPickerDialog.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.TextView;
-
 import static com.tb_system.todolist.Cont.*;
 
 /**
